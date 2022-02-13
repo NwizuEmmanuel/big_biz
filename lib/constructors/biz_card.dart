12e7dfd7ad8a80f74c0c0b097a12b3b929533989
NwizuEmmanuel <nwizuemmanuel200@gmail.com>
2022-02-13 22:44:18 +0100

    initial commit

--- lib/constructors/biz_card.dart
+++
@@ -1,7 +0,0 @@
-class BizCard{
-  late String brandImage;
-  late String bizName;
-  late String bizDetails;
-
-  BizCard({required this.brandImage, required this.bizName, required this.bizDetails});
-}
